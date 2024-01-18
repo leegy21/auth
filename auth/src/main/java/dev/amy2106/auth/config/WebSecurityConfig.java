18ff72b95e9b4221d4bba78ea66b2e616be3a0bf
leegy21 <amy2106@naver.com>
2024-01-18 16:42:17 +0900

    first commit

---
+++ auth/src/main/java/dev/amy2106/auth/config/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package dev.amy2106.auth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/home/**")
+                .anonymous()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/user/login")
+                .defaultSuccessUrl("/home")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/user/logout")
+                .logoutSuccessUrl("/home")
+                .deleteCookies("JSEESIONID")
+                .invalidateHttpSession(true)
+                .permitAll()
+        ;
+    }
+}
---
+++ auth/src/test/java/dev/amy2106/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.amy2106.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
